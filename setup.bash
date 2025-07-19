bd478805660e4c4a0fd53c156971ad4818d9d80c
Cameron Brown <savildatechnology@gmail.com>
2025-07-18 22:26:56 -0600

    fixed setup script

--- setup.bash
+++ setup.bash
@@ -2,7 +2,10 @@
 [ $(which wget) == "" ] && echo "you need wget for this installer to work" && exit 1
 
 rm go-discover
+rm run-discover.bash
+
 wget https://github.com/savildatech/go-discover/raw/refs/heads/main/go-discover
 wget https://github.com/savildatech/go-discover/raw/refs/heads/main/run-discover.bash
+
 chmod +x go-discover
 chmod +x run-discover
