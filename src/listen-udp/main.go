2431bd24ad3befa826fb49f8ff5469afe7692393
Cameron Brown <savildatechnology@gmail.com>
2025-07-21 11:04:55 -0600

    fixed listener

--- src/listen-udp/main.go
+++ src/listen-udp/main.go
@@ -4,50 +4,23 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 )
 
 func main() {
-	ifaceName := os.Getenv("INTERFACE")
-	if ifaceName == "" {
-		fmt.Println("INTERFACE env var required")
-		os.Exit(1)
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
 		fmt.Println("PORT env var required")
 		os.Exit(1)
 	}
 
-	iface, err := net.InterfaceByName(ifaceName)
+	port, err := strconv.Atoi(portStr)
 	if err != nil {
-		fmt.Printf("Error finding interface: %v\n", err)
+		fmt.Printf("Invalid port: %v\n", err)
 		os.Exit(1)
 	}
 
-	addrs, err := iface.Addrs()
-	if err != nil {
-		fmt.Printf("Error getting addresses: %v\n", err)
-		os.Exit(1)
-	}
-
-	var ip net.IP
-	for _, addr := range addrs {
-		if ipNet, ok := addr.(*net.IPNet); ok && ipNet.IP.To4() != nil {
-			ip = ipNet.IP
-			break
-		}
-	}
-	if ip == nil {
-		fmt.Println("No IPv4 address found on interface")
-		os.Exit(1)
-	}
-
-	addr := &net.UDPAddr{
-		IP:   ip,
-		Port: atoi(port),
-	}
-
+	addr := &net.UDPAddr{IP: net.ParseIP("0.0.0.0"), Port: port}
 	conn, err := net.ListenUDP("udp", addr)
 	if err != nil {
 		fmt.Printf("Error listening: %v\n", err)
@@ -64,12 +37,4 @@ func main() {
 		}
 		fmt.Printf("Received %d bytes from %s: %s\n", n, remoteAddr, string(buf[:n]))
 	}
-}
-
-func atoi(s string) int {
-	i := 0
-	for _, c := range s {
-		i = i*10 + int(c-'0')
-	}
-	return i
 }
