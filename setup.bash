860d2c22d912646c1835acd3abf3f50a50629a39
Cameron Brown <savildatechnology@gmail.com>
2025-07-21 10:07:35 -0600

    fixed scripts

--- setup.bash
+++ setup.bash
@@ -1,11 +1,9 @@
 #!/bin/bash
-[ $(which wget) == "" ] && echo "you need wget for this installer to work" && exit 1
 
-rm go-discover
-rm run-discover.bash
+execs=("setup.bash" "bin/broadcast-svc" "bin/listen-udp")
 
-wget https://github.com/savildatech/go-discover/raw/refs/heads/main/go-discover
-wget https://github.com/savildatech/go-discover/raw/refs/heads/main/run-discover.bash
-
-chmod +x go-discover
-chmod +x run-discover
+for exec in "${execs[@]}"; do
+    rm "$exec"
+    wget "https://github.com/savildatech/go-discover/raw/refs/heads/main/$exec"
+    chmod +x "$exec"
+done
